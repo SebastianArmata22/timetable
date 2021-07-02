4f78abcf223a195e4318432929692615ce4e6f6a
SebastianArmata22 <62599102+SebastianArmata22@users.noreply.github.com>
2021-07-02 16:29:29 +0200

    add a state availabilites

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import Hours from 'react-hours'
+import React, { useState } from 'react'
+import SchedulerCalendar from 'scheduler-calendar'
+import 'scheduler-calendar/dist/index.css'
 import './schedule.scss'
 const Schedule = () => {
-    return (
-        <div className="schedule">
-            <Hours theme='bootstrap' value="We 11:00-12:00"/>
+    const [availabilities, setAvailabilities]=useState([
+        {
+          day: "mon",
+          slots: [
+            {from: '09:00', to: '10:30'},
+            {from: '11:30', to: '13:00'},
+            {from: '14:30', to: '17:00'},
+          ]
+        },
+        {
+          day: "2021-07-03",
+          slots: [
+            {from: '09:00', to: '10:30'},
+            {from: '11:30', to: '19:00'},
+          ]
+        }
+      ])
+      return (
+        <div className="App">
+          <SchedulerCalendar
+                  availabilities={availabilities}
+                  availabilityType={'infinity'}
+                  duration={10}
+                  onIntervalChange={() => {}}
+                />
         </div>
-
-    )
+      )
 }
 
 export default Schedule
