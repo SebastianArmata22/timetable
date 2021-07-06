a04c0630faf7b2768d5b68ae3d1886ea7b7d826b
SebastianArmata22 <62599102+SebastianArmata22@users.noreply.github.com>
2021-07-06 09:29:13 +0200

    add connection to database in schedule

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../NavBar/NavBar'
 import Counter from '../counter/Counter'
 import Schedule from '../schedule/Schedule'
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { auth, database } from '../../firebase/config';
 const Account = () => {
+    const { uid } = auth.currentUser
+    console.log(uid)
+    const queryToBase = database.collection("users").doc(uid).collection('schedules')
+    const [schedules] = useCollectionData(queryToBase)
+    console.log(schedules, "sss")
+    const changeSchedule=async (schedule)=>{
+        await database.collection('users').doc(uid).collection('schedules').add(schedule[0])
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    }
     return (
         <div>
             <NavBar />
             <Counter />
-            <Schedule />
+            <Schedule schedule={schedules? schedules:[]} changeSchedule={changeSchedule}/>
         </div>
     )
 }
--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -3,25 +3,15 @@ import SchedulerCalendar from 'scheduler-calendar'
 import 'scheduler-calendar/dist/index.css'
 import './schedule.scss'
 
-const Schedule = () => {
-    const [availabilities, setAvailabilities]=useState([
-      {
-        day: "2021-01-26",
-        slots: [
-          {from: '09:00', to: '10:30'},
-          {from: '11:30', to: '19:00'},
-        ]
-      }
-    ])
+const Schedule = ({schedule,changeSchedule}) => {
       return (
         <div className="App">
           <SchedulerCalendar
-                  availabilities={availabilities}
+                  availabilities={schedule}
                   availabilityType={'infinity'}
                   duration={10}
                   is24hour={true}
-                  onIntervalChange={(data) => {setAvailabilities(data)
-                  console.log(data)}}
+                  onIntervalChange={(data) => {changeSchedule(data)}}
                 />
         </div>
       )
